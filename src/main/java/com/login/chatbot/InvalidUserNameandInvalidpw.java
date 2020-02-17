cbba2c1e1f6cb4d18b67223f698f92a6dc832ee2
nilushijayasinghe <prabodika91@gmail.com>
2020-02-17 14:10:53 +0530

    configurations changes.

--- src/main/java/com/login/chatbot/InvalidUserNameandInvalidpw.java
+++ src/main/java/com/login/chatbot/InvalidUserNameandInvalidpw.java
@@ -19,9 +19,9 @@ public class InvalidUserNameandInvalidpw {
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception{
         System.setProperty("webdriver.chrome.driver","/home/nilushi/Documents/chatbot/Chat_Bot_Regeression_Cycle/libs/chromedriver");
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("headless");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
